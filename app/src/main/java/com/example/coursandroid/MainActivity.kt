72a6603499eb39eb8709123670dbd893317bfb39
mustapha <elmustaphaes.salmi@gmail.com>
2025-10-08 14:43:29 +0100

    ajouter circle et point

--- app/src/main/java/com/example/coursandroid/MainActivity.kt
+++ app/src/main/java/com/example/coursandroid/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.coursandroid
 
-import android.R
+import java.util.Date
+import kotlin.math.pow
 
+class Point(val x : Float,val y : Float)
+
+class circle(val centre:Point, val rayon: Float){
+
+    fun getSurface() : Double{
+        return 2* 3.14 * rayon.pow(2)
+    }
+}
 open class voiture(var mar: String, var mat: String, var num:Int){
 
      fun afficher(){
         println("c est une $mar");
     }
 
-    fun getNum(): Int {
+    fun getNumber(): Int {
         return num*2
     }
 }
 
 class camion( mar: String,  mat: String,  num:Int, var poids : Float) : voiture(mar,mat,num) {
+
 
 
 }
@@ -22,8 +32,12 @@ fun main(){
     var v = voiture("ford","Z344",12)
     var c = camion("totyota","Z344",12,123f)
 
+    var point = Point(2f,3f)
+    var cir = circle(point,5f)
+
     v.afficher()
-    var numr =  v.getNum()
+    var numr =  v.getNumber()
+    println(numr)
 
 
 
