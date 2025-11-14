390ba1bfa10432d441bc0debf552d6370a15fcd0
mustapha <elmustaphaes.salmi@gmail.com>
2025-11-14 10:54:53 +0100

    filter by

--- app/src/main/java/com/example/coursandroid/filterby.kt
+++ app/src/main/java/com/example/coursandroid/filterby.kt
@@ -9,17 +9,17 @@ enum class Attraction{
 
 class Ville(val nom : String, val listAttractions : ArrayList<Attraction>){
 
-    override fun toString(): String {
-        var listatt = ""
-        for(attrac in listAttractions){
-            listatt = "$listatt  $attrac"
-        }
-        return "ville : $nom "
-    }
+     override fun toString(): String {
+         var listatt = ""
+         for(attrac in listAttractions){
+             listatt = "$listatt  $attrac"
+         }
+         return "ville : $nom les attractions : $listatt"
+     }
 
-    override fun equals(other: Any?): Boolean {
-        return this == other
-    }
+    /* override fun equals(other: Any?): Boolean {
+         return this == other
+     }*/
 }
 
 class Circuit(val numero : Int, val description : String,val prix : Double){
@@ -49,10 +49,23 @@ class Circuit(val numero : Int, val description : String,val prix : Double){
 }
 fun main(){
     val ville1 = Ville("Casabnaca",arrayListOf(Attraction.KESBAH, Attraction.MONTAGNE))
-    val listattraction = arrayListOf(Attraction.KESBAH, Attraction.MONTAGNE)
+    val listattraction = arrayListOf(Attraction.KESBAH, Attraction.MEDINA)
     val ville2 = Ville("Rabat",listattraction)
     val circuit1 = Circuit(1,"circuit casa",500.0)
+    val circuit2 = Circuit(2,"circuit Rabat",1000.0)
     circuit1.ajouterVille(ville1)
     circuit1.ajouterVille(ville2)
+    circuit2.ajouterVille(ville2)
     circuit1.afficherCircuit()
+    var lstCircuits : ArrayList<Circuit> = arrayListOf()
+    lstCircuits.add(circuit1)
+    lstCircuits.add(circuit2)
+    println(lstCircuits.size)
+    val listprixsup8 = lstCircuits.filter{it.prix > 800}
+    // le circuit de plus grand prix
+    val prixpluschere = lstCircuits.maxBy { it.prix }
+    // le circuit de plus grand numero
+    val circuitplusgrandnumer = lstCircuits.maxBy { it.numero }
+    println(prixpluschere.prix)
+
 }
